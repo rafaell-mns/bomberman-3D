26ec16f5e4d5adc9b3aef927812c5177f5cd979b
Bernardoc14 <147110226+Bernardoc14@users.noreply.github.com>
2025-01-12 23:59:44 -0300

    Update main.cpp
    consegui pensar numa forma de fazer as animacoes, por enquanto só tem para andar pra frente com o W. Logo eu atualizo fazendo pras outras direções, foi mais trabalhoso do que eu pensava.

--- main.cpp
+++ main.cpp
@@ -15,14 +15,16 @@
 #include <set>
 #include <utility>
 #include <cstdio>
-
+#include <conio.h> 
+#include <iostream>
+#include <vector>
 #define N 1 	// muro normal
 #define M 2 	// muro com musgo
 #define C 3 	// caixote
 
 //Largura e altura da janela
 int width = 800, height = 500;
-
+float flag = true;
 //Coordenadas da posicao atual do mouse
 int m_x, m_y;
 
@@ -40,8 +42,8 @@ int matrizMapa[LINHAS_MAPA][COLUNAS_MAPA] = {
 	{N, M, N, N, M, M, N, N, M, M, N, M, N, M, N, N}
 };
 
-std::set<std::pair<int, int> > muro;   // Conjunto para armazenar as posições de colisão com o muro
-std::set<std::pair<int, int> > caixa;  // Conjunto para armazenar as posições de colisão com as caixas
+std::set<std::pair<int, int> > muro;   // Conjunto para armazenar as posi??es de colis?o com o muro
+std::set<std::pair<int, int> > caixa;  // Conjunto para armazenar as posi??es de colis?o com as caixas
 
 bool temColisao(int x, int y, const std::set< std::pair<int, int> >& colisoes) {
     // Iterar por todos os pares de coordenadas no set
@@ -62,7 +64,7 @@ void colisoesMatriz(){
     // Iterar por todas as linhas e colunas da matriz
     for (int z = 0; z < LINHAS_MAPA; z++) {
         for (int x = 0; x < COLUNAS_MAPA; x++) {
-            // Se o valor na matriz for 'M' ou 'N', calcular todas as coordenadas dentro da célula
+            // Se o valor na matriz for 'M' ou 'N', calcular todas as coordenadas dentro da c?lula
             if (matrizMapa[z][x] == M || matrizMapa[z][x] == N) {
                 // Para cada bloco de muro, iterar dentro dele (4x4 unidades)
                 for (int i = 0; i < 4; ++i) {
@@ -88,7 +90,7 @@ void colisoesMatriz(){
     }
 }
 
-// Protótipos das funções
+// Prot?tipos das fun??es
 void drawSphere(float radius, float r, float g, float b);
 void drawCube(float size, float r, float g, float b);
 void moverPersonagem(float novoX, float novoZ);
@@ -96,32 +98,242 @@ void mousePassiveMotion(int x, int y);
 
 float personagemX = 0.0f, personagemZ = 0.0f;
 float anguloRotacao = 0.0f; 
+bool andando = false;
+bool valorW = true;
+std::vector<float> v;
 
-// Função para desenhar o personagem Bomberman
-void drawBomberman(){
-	glPushMatrix();
-		// mexer na posição do player
+void preencherVetor(std::vector<float>& v) {
+    for (float i = 0.17f; i >= -0.17f; i -= 0.05f) {
+        v.push_back(i);
+    }
+}
+
+float k = 0;
+float t = 0;
+
+// FunÃ§Ã£o para desenhar o personagem Bomberman
+void andarBomberman(float t) {
+		// alternar  = true
+		// perna direita pra tras e braÃ§o esquerdo pra frente
+		// alternar = false
+		// perna esquerda pra tras e braÃ§o direito pra frente
+		
+ glPushMatrix();
+ 	 
+		// mexer na posi??o do player
 		glTranslatef(-28+personagemX, 3.7, 0.0f+personagemZ);
 		//glRotated(10,anguloRotacao,anguloRotacao,anguloRotacao); > talvez fique bom quando adicionar as animacoes
 		float e = 2;
+		
+		glRotated(180,0,1,0);
 		glScaled(e, e, e);
 		
-		// Função para desenhar o personagem Bomberman
+		// Fun??o para desenhar o personagem Bomberman
 		glPushMatrix();
 		// mexer no rosto + cabeca + olhos
 		glTranslatef(0, -0.2, 0);
 		glScaled(1.1, 1.1, 1.1);
+		
 
-		//Cabeça
+		//Cabe?a
 		glPushMatrix();
 		glTranslatef(0.0f, 1.2f, 0.0f);
-		drawSphere(0.5f, 1.0f, 1.0f, 1.0f); // Cabeça branca
+		drawSphere(0.5f, 1.0f, 1.0f, 1.0f); // Cabe?a branca
 		glPopMatrix();
 
 		// rosto
 		glPushMatrix();
 		glTranslatef(0.0f, 1.21f, 0.08);
+		 e = 0.9;
+		glScaled(e, e, e);
+		drawSphere(0.5, 0.961, 0.741, 0.569); // rosto
+		glPopMatrix();
+
+		// olhos
+		glPushMatrix();
+		glTranslatef(-0.15, 1.21f, 0.32);
+		e = 0.6;
+		glScaled(0.2, 1, 1);
+		drawCube(0.4f, 0, 0, 0); // Perna esquerda vermelha
+		glPopMatrix();
+
+		glPushMatrix();
+		glTranslatef(0.15, 1.21f, 0.32);
+		e = 0.6;
+		glScaled(0.2, 1, 1);
+		drawCube(0.4f, 0, 0, 0); // Perna esquerda vermelha
+		glPopMatrix();
+		glPopMatrix();
+		
+		// Corpo
+		glPushMatrix();
+		glTranslatef(0.0f, 0.3, 0.0f);
+		glScaled(1, 1, 1);
+		drawCube(0.7f, 0.0f, 0.0f, 1.0f); // Corpo azul
+		glPopMatrix();
+	
+		// fundo do cinto
+		glPushMatrix();
+		glTranslatef(0.0f, 0.1, 0.35f);
+		e = 0.6;
+		glScaled(1.8, 0.3, 0.1);
+		drawCube(0.4f, 0, 0, 0); // Perna esquerda vermelha
+		glPopMatrix();
+	
+		// parte dourada do cinto cireita
+		glPushMatrix();
+		glTranslatef(0.1, 0.1, 0.38f);
+		e = 0.2;
+		glScaled(0.05, 0.2, e);
+		drawCube(0.4f, 1, 1, 0); // Perna esquerda vermelha
+		glPopMatrix();
+	
+		// parte dourada do cinto esquerda
+		glPushMatrix();
+		glTranslatef(-0.1, 0.1, 0.38f);
+		e = 0.2;
+		glScaled(0.05, 0.2, e);
+		drawCube(0.4f, 1, 1, 0); // Perna esquerda vermelha
+		glPopMatrix();
+	
+		// parte dourada do cinto cima
+		glPushMatrix();
+		glTranslatef(0, 0.13, 0.38f);
+		e = 0.2;
+		glScaled(0.45, 0.05, e);
+		drawCube(0.4f, 1, 1, 0); // Perna esquerda vermelha
+		glPopMatrix();
+	
+		// parte dourada do cinto cima
+		glPushMatrix();
+		glTranslatef(0, 0.07, 0.38f);
+		e = 0.2;
+		glScaled(0.45, 0.05, e);
+		drawCube(0.4f, 1, 1, 0); // Perna esquerda vermelha
+		glPopMatrix();
+
+		// anteperna esquerda
+		glPushMatrix();
+		glTranslatef(-0.2, -0.2, 0.0f);
 		e = 0.9;
+		glScaled(e, 1.25, e);
+		glRotated(45,-1,0,0);
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
+		glPopMatrix();
+	
+		
+	
+	
+		
+		// Perna  esquerda
+		glPushMatrix();
+		glTranslatef(-0.2f, -0.4, -t);
+		glRotated(45,-1,0,0);
+		e = 0.9;
+		glScaled(e, e, e);
+		drawCube(0.4f, 1.0f, 0.0f, 0.0f); // Perna esquerda vermelha
+		glPopMatrix();
+	
+		// anteperna direita
+		glPushMatrix();
+		glTranslatef(0.2, -0.2, 0.0f);
+		e = 0.9;
+		glScaled(e, 1.25, e);
+		glRotated(45,-1,0,0);
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
+		glPopMatrix();
+	
+		
+		// Perna  direita
+		glPushMatrix();
+		glTranslatef(0.2f, -0.4, t);
+		glRotated(45,-1,0,0);
+		glScaled(e, e, e);
+		drawCube(0.4f, 1.0f, 0.0f, 0.0f); // Perna esquerda vermelha
+		glPopMatrix();
+
+	
+		// mexer nos bra?os
+glPushMatrix();
+		glTranslatef(0.0f, -0.34, 0.0f);
+
+		// antebra?o esquerdo
+		glPushMatrix();
+		glTranslatef(-0.5f, 0.8f, 0.0f);
+		e = 0.9;
+		glRotated(45, -1, 0, 0);      
+		glScaled(e, e, e);
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
+		glPopMatrix();
+	
+	
+
+		// Bra?o esquerdo
+		glPushMatrix();
+		glTranslatef(-0.5, 0.63, t);
+		glRotated(45, -1, 0, 0);        
+		drawCube(0.3f, 1, 0, 0); // Bra?o esquerdo amarelo
+		glPopMatrix();
+	
+		// antebra?o direito
+		glPushMatrix();
+		glTranslatef(0.5f, 0.8f, 0.0f);
+		e = 0.9;
+		glRotated(45, -1, 0, 0);  
+		glScaled(e, e, e);
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
+		glPopMatrix();
+	
+		// Bra?o direito
+		glPushMatrix();
+		glTranslatef(0.5, 0.63, -t);
+		glRotated(45, -1, 0, 0);  
+		drawCube(0.3f, 1, 0, 0); // Bra?o esquerdo amarelo
+		glPopMatrix();
+glPopMatrix();
+		// Antena
+		glPushMatrix();
+		glTranslatef(0.0f, 1.8f, 0.0f);
+		drawSphere(0.1f, 1.0f, 0.0f, 0.0f); // Bolinha vermelha na antena
+		glPopMatrix();
+	
+		glPushMatrix();
+		glTranslatef(0.0f, 1.5f, 0.0f);
+		glScalef(0.1f, 0.5f, 0.1f);
+		drawCube(1.0f, 0.5f, 0.5f, 0.5f); // Haste da antena cinza
+		glPopMatrix();
+glPopMatrix();
+}
+
+
+
+
+// Fun??o para desenhar o personagem Bomberman
+void drawBomberman(){
+	
+glPushMatrix();
+		// mexer na posi??o do player
+		glTranslatef(-28+personagemX, 3.7, 0.0f+personagemZ);
+		//glRotated(10,anguloRotacao,anguloRotacao,anguloRotacao); > talvez fique bom quando adicionar as animacoes
+		float e = 2;
+		glScaled(e, e, e);
+		
+		// Fun??o para desenhar o personagem Bomberman
+		glPushMatrix();
+		// mexer no rosto + cabeca + olhos
+		glTranslatef(0, -0.2, 0);
+		glScaled(1.1, 1.1, 1.1);
+
+		//Cabe?a
+		glPushMatrix();
+		glTranslatef(0.0f, 1.2f, 0.0f);
+		drawSphere(0.5f, 1.0f, 1.0f, 1.0f); // Cabe?a branca
+		glPopMatrix();
+
+		// rosto
+		glPushMatrix();
+		glTranslatef(0.0f, 1.21f, 0.08);
+		  e = 0.9;
 		glScaled(e, e, e);
 		drawSphere(0.5, 0.961, 0.741, 0.569); // rosto
 		glPopMatrix();
@@ -189,13 +401,14 @@ void drawBomberman(){
 		drawCube(0.4f, 1, 1, 0); // Perna esquerda vermelha
 		glPopMatrix();
 	
-	
+		
 		// anteperna esquerda
 		glPushMatrix();
 		glTranslatef(-0.2, -0.2, 0.0f);
+		//glRotated(45, -1,0,0);
 		e = 0.9;
 		glScaled(e, 1.25, e);
-		drawCube(0.3f, 1, 1, 1); // Braço esquerdo amarelo
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
 		glPopMatrix();
 	
 		// Perna  esquerda
@@ -211,7 +424,7 @@ void drawBomberman(){
 		glTranslatef(0.2, -0.2, 0.0f);
 		e = 0.9;
 		glScaled(e, 1.25, e);
-		drawCube(0.3f, 1, 1, 1); // Braço esquerdo amarelo
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
 		glPopMatrix();
 	
 		// Perna  direita
@@ -222,35 +435,39 @@ void drawBomberman(){
 		glPopMatrix();
 	
 	
-		// mexer nos braços
+		// mexer nos bra?os
 		glPushMatrix();
-		glTranslatef(0, -0.34, 0);
-		// antebraço esquerdo
+		
+		glTranslatef(0, -0.2, 0.5);
+		glRotated(45,-1,0,0);
+		
+	
+		// antebra?o esquerdo
 		glPushMatrix();
 		glTranslatef(-0.5f, 0.8f, 0.0f);
 		e = 0.9;
 		glScaled(e, e, e);
-		drawCube(0.3f, 1, 1, 1); // Braço esquerdo amarelo
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
 		glPopMatrix();
 	
-		// Braço esquerdo
+		// Bra?o esquerdo
 		glPushMatrix();
-		glTranslatef(-0.8, 0.8f, 0.0f);
-		drawCube(0.3f, 1, 0, 0); // Braço esquerdo amarelo
+		glTranslatef(-0.5, 0.55, 0.0f);
+		drawCube(0.3f, 1, 0, 0); // Bra?o esquerdo amarelo
 		glPopMatrix();
 	
-		// antebraço direito
+		// antebra?o direito
 		glPushMatrix();
 		glTranslatef(0.5f, 0.8f, 0.0f);
 		e = 0.9;
 		glScaled(e, e, e);
-		drawCube(0.3f, 1, 1, 1); // Braço esquerdo amarelo
+		drawCube(0.3f, 1, 1, 1); // Bra?o esquerdo amarelo
 		glPopMatrix();
 	
-		// Braço direito
+		// Bra?o direito
 		glPushMatrix();
-		glTranslatef(0.8, 0.8f, 0.0f);
-		drawCube(0.3f, 1, 0, 0); // Braço esquerdo amarelo
+		glTranslatef(0.5, 0.55, 0.0f);
+		drawCube(0.3f, 1, 0, 0); // Bra?o esquerdo amarelo
 		glPopMatrix();
 		glPopMatrix();
 		// Antena
@@ -264,7 +481,7 @@ void drawBomberman(){
 		glScalef(0.1f, 0.5f, 0.1f);
 		drawCube(1.0f, 0.5f, 0.5f, 0.5f); // Haste da antena cinza
 		glPopMatrix();
-	glPopMatrix();
+glPopMatrix();
 }
 
 
@@ -295,7 +512,7 @@ void display()
 	// Limpar os buffers de cor e profundidade
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	// Configurar a câmera
+	// Configurar a cÃ¢mera
 	glLoadIdentity();
 	gluLookAt(eyeX, eyeY, eyeZ, centerX, centerY, centerZ, 0, 1, 0);
 
@@ -314,14 +531,32 @@ void display()
 	glDisable(GL_TEXTURE_2D);   // Desativar texturas para o texto
 	glColor3f(1.0f, 1.0f, 1.0f); // Garantir cor branca para o texto
 	draw_text_stroke(-20, 20, "(" + to_string(m_x) + "," + to_string(m_y) + ")", 0.01);
-	drawBomberman();
-	glEnable(GL_TEXTURE_2D);    // Reativar texturas caso necessário
+
+
+
+	if (andando){
+		andarBomberman(t);	
+	}		
+	else{
+		drawBomberman();
+	}
+	    
+		
+     
+			
+	
+		
+	
+	
+	
+	glEnable(GL_TEXTURE_2D);    // Reativar texturas caso necessÃ¡rio
 	glPopMatrix();
 
-	// Finalizar a renderização
+	// Finalizar a renderizaÃ§Ã£o
 	glFlush();
 	glutSwapBuffers();
 }
+
 
 
 
@@ -342,6 +577,7 @@ void drawSphere(float radius, float r, float g, float b)
 // Funcao para capturar entradas do teclado
 void teclado(unsigned char key, int x, int y)
 {
+	
 	const float movimento = 1.0; // Define a quantidade de movimento
 	switch (key)
 	{
@@ -384,9 +620,21 @@ void teclado(unsigned char key, int x, int y)
 		if (!temColisao(personagemX, personagemZ - 1, muro) && !temColisao(personagemX, personagemZ - 1, caixa)){
 			centerZ -= movimento;
 			eyeZ-=movimento;
-			
 			personagemZ -= movimento;
+			andando = true;
+			if (k >= v.size())
+				flag = false;
+			if (k <= 0)
+				flag = true;
+			t = v[k];
+			printf("t: %f ",t);
+			if (flag)
+				k++;	
+            else
+				k--;
+			
 			printf("(%.0f, %.0f)\n", personagemX, personagemZ);
+			
 			
 			anguloRotacao = 180.0f;
 		}
@@ -470,20 +718,55 @@ void teclado(unsigned char key, int x, int y)
 
 	case 'p':
 	case 'P':
-
 		printf("Acabou");
 		return;
 		break;
 
 	default:
+		
+		
 		break;
 	}
+	
+	
 
 	glutPostRedisplay();
 }
 
+
+void tecladoSolta(unsigned char key, int x, int y){
+	
+		switch (key)
+	{
+
+	case 'w':
+	case 'W': // Mover o personagem pra frente
+		andando = false;
+
+	case 's':
+	case 'S': // Mover o personagem pra tras
+
+		break;
+	case 'a':
+	case 'A': // Mover o personagem pra esquerda
+
+	case 'd':
+	case 'D': // Mover o personagem pra direita
+
+
+	default:
+		
+		
+		break;
+	}
+	
+	
+
+	glutPostRedisplay();
+	
+}
 void init()
-{
+{ 
 	glClearColor(0.4, 0.7, 1, 1); // Cor de fundo azul claro
 	//configurarIluminacao();         // Configura a iluminacao
 
@@ -514,6 +797,7 @@ void mousePassiveMotion(int x, int y)
 
 int main(int argc, char** argv)
 {
+	preencherVetor(v);
 	// Inicializa o GLUT e configura a janela
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
@@ -523,6 +807,7 @@ int main(int argc, char** argv)
 	glutDisplayFunc(display);
 	glutReshapeFunc(redimensiona);
 	glutKeyboardFunc(teclado);
+	glutKeyboardUpFunc(tecladoSolta); 
 	glutPassiveMotionFunc(mousePassiveMotion); //fucao callback do movimento passivo do mouse
 	printf("W/w: Mover para frente\n");
 	printf("S/s: Mover para tras\n");
